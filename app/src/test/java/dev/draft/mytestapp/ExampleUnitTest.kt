0bb1f5720579c19a19ac4cf027dab4d8893b1de6
Kumar Harsh <kharsh39@gmail.com>
2024-03-23 14:30:41 +0530

    Add basic unit tests

--- app/src/test/java/dev/draft/mytestapp/ExampleUnitTest.kt
+++ app/src/test/java/dev/draft/mytestapp/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package dev.draft.mytestapp
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.ArithmeticException
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +15,26 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun subtraction_isCorrect() {
+        assertEquals(0, 2-2)
+    }
+
+    @Test
+    fun division_isCorrect() {
+        assertEquals(1, 2/2)
+    }
+
+    @Test
+    fun multiplication_isCorrect() {
+        assertEquals(4, 2*2)
+    }
+
+    @Test(expected = ArithmeticException::class)
+    fun division_byZeroIsUndefined() {
+        2/0
+    }
+
+
 }
